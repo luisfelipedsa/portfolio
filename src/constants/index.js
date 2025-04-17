9116b97b437d02347414cdf4967dff8e6ae1bb2a
Luis Felipe <luisfelipedas@gmail.com>
2025-04-17 01:01:53 -0300

    Apenas detalhes de links

--- src/constants/index.js
+++ src/constants/index.js
@@ -239,18 +239,18 @@ const testimonials = [
 ];
 
 const socialImgs = [
-  {
-    name: "insta",
-    imgPath: "/images/insta.png",
-  },
-  {
-    name: "fb",
-    imgPath: "/images/fb.png",
-  },
-  {
-    name: "x",
-    imgPath: "/images/x.png",
-  },
+  // {
+  //   name: "insta",
+  //   imgPath: "/images/insta.png",
+  // },
+  // {
+  //   name: "fb",
+  //   imgPath: "/images/fb.png",
+  // },
+  // {
+  //   name: "x",
+  //   imgPath: "/images/x.png",
+  // },
   {
     name: "linkedin",
     imgPath: "/images/linkedin.png",
